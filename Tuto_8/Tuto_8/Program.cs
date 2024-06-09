17c13cd7995b9da354d0b031ca95b27775ea22cc
s28210 <s28210@pjwstk.edu.pl>
2024-06-09 22:28:20 +0200

    registering services

--- Tuto_8/Tuto_8/Program.cs
+++ Tuto_8/Tuto_8/Program.cs
@@ -1,4 +1,9 @@
 
+using Microsoft.EntityFrameworkCore;
+using Tuto_8.Interfaces;
+using Tuto_8.Models;
+using Tuto_8.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +12,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<MasterContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IAssignClientToTripRepository, AssignClientToTripRepository>();
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<ITripRepository, TripRepository>();
+
 
 var app = builder.Build();
 
